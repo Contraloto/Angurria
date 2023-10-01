0cf79cc6439454869203c31433f0046f9cd72f22
Contraloto <144296311+Contraloto@users.noreply.github.com>
2023-10-01 16:10:06 -0500

    Update Angurria2.py

--- Angurria2.py
+++ Angurria2.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Página principal
 st.title("Página Principal")
-st.image("", caption="Imagen de ejemplo")
+st.image(r"https://w7.pngwing.com/pngs/361/694/png-transparent-light-circle-geometry-science-and-technology-blue-mechanical-blue-angle-electronics.png", caption="Imagen de ejemplo")
 
 if st.button("Ir a la otra página"):
     st.write("Redirigiendo a la otra página...")
