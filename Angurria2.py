82e549c161f1f81fe4e0d801628561b3d56e185e
Contraloto <144296311+Contraloto@users.noreply.github.com>
2023-10-01 16:52:44 -0500

    Update Angurria2.py

--- Angurria2.py
+++ Angurria2.py
@@ -1,10 +1,28 @@
 import streamlit as st
 
+# Variable para el estado de redirección
+redireccionado = False
+
 # Página principal
 st.title("Página Principal")
-st.image(r"https://w7.pngwing.com/pngs/361/694/png-transparent-light-circle-geometry-science-and-technology-blue-mechanical-blue-angle-electronics.png", caption="Imagen de ejemplo")
 
-if st.button("Ir a la otra página"):
-    st.write("Redirigiendo a la otra página...")
-    # Redirigir al usuario a otra página
-    st.experimental_set_query_params(pagina="otra")
+# Verificar si se ha redirigido a la página secundaria
+if "pagina" in st.experimental_get_query_params():
+    if st.experimental_get_query_params()["pagina"] == "otra":
+        redireccionado = True
+
+if redireccionado:
+    st.empty()  # Limpiar la pantalla principal
+    st.title("Página Secundaria")
+    st.write("Bienvenido a la página secundaria")
+    
+    # Botón para regresar a la página principal
+    if st.button("Regresar a la página principal"):
+        st.experimental_set_query_params(pagina="principal")  # Redirigir de nuevo a la página principal
+else:
+    st.image("tu_imagen.jpg", caption="Imagen de ejemplo")
+
+    if st.button("Ir a la otra página"):
+        st.write("Redirigiendo a la otra página...")
+        st.experimental_set_query_params(pagina="otra")
+
