ed595f57967bd6bc5afab5b198bba5b9496cfad6
Contraloto <144296311+Contraloto@users.noreply.github.com>
2023-10-01 18:38:08 -0500

    Update Angurria2.py

--- Angurria2.py
+++ Angurria2.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import random
 
 # Diccionario para almacenar las salas y sus códigos
 salas = {}
@@ -11,33 +10,27 @@ st.title("Sala de Chat")
 accion = st.radio("Selecciona una acción:", ("Crear una sala", "Unirse a una sala"))
 
 if accion == "Crear una sala":
-    # Genera un código de sala aleatorio
-    codigo_sala = random.randint(1000, 9999)
-    st.write(f"Tu código de sala es: {codigo_sala}")
     sala_id = st.text_input("Ingresa un nombre para la sala:")
     if st.button("Crear Sala"):
         if sala_id:
+            codigo_sala = str(hash(sala_id))
             salas[codigo_sala] = {"nombre_sala": sala_id, "mensajes": []}
-            st.success(f"Sala '{sala_id}' creada con éxito.")
+            st.balloons()
+            st.success(f"Sala '{sala_id}' creada con éxito. Código de sala: {codigo_sala}")
 
 elif accion == "Unirse a una sala":
     codigo_ingresado = st.text_input("Ingresa el código de la sala:")
     if st.button("Unirse a Sala"):
-        if codigo_ingresado:
-            if int(codigo_ingresado) in salas:
-                sala_actual = salas[int(codigo_ingresado)]
-                st.write(f"Te has unido a la sala '{sala_actual['nombre_sala']}'")
-                st.write(f"Código de sala: {codigo_ingresado}")
-                st.write("Mensajes en la sala:")
-                mensajes_sala = sala_actual["mensajes"]
-                for mensaje in mensajes_sala:
-                    st.write(mensaje)
-            else:
-                st.error("Código de sala no válido. Inténtalo de nuevo.")
+        if codigo_ingresado in salas:
+            st.balloons()
+            st.success(f"Te has unido a la sala '{salas[codigo_ingresado]['nombre_sala']}'")
 
 # Chat en la sala (puede ser mejorado para incluir más características)
-mensaje = st.text_area("Escribe un mensaje:")
-if st.button("Enviar"):
-    if accion == "Unirse a una sala" and int(codigo_ingresado) in salas:
-        sala_actual["mensajes"].append(f"Usuario: {mensaje}")
+if accion == "Unirse a una sala" and codigo_ingresado in salas:
+    sala_actual = salas[codigo_ingresado]
+    mensaje = st.text_area("Escribe un mensaje:")
+    if st.button("Enviar"):
+        if mensaje:
+            sala_actual["mensajes"].append(f"Usuario: {mensaje}")
+
 
