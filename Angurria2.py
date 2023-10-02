9ff1f2920d1feb01555393216d009f8f0ce5ebd3
Contraloto <144296311+Contraloto@users.noreply.github.com>
2023-10-01 19:09:04 -0500

    Update Angurria2.py

--- Angurria2.py
+++ Angurria2.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import random
+import time
 
 # Diccionario para almacenar las salas y sus códigos
 salas = {}
@@ -7,29 +8,41 @@ salas = {}
 # Título de la aplicación
 st.title("Sala de Chat")
 
+# Agrega el CSS personalizado
+st.markdown(
+    '<link rel="stylesheet" href="css/custom.css">',
+    unsafe_allow_html=True
+)
+
 # Selección de acción: Crear sala o unirse a sala
 accion = st.radio("Selecciona una acción:", ("Crear una sala", "Unirse a una sala"))
 
 if accion == "Crear una sala":
     sala_id = st.text_input("Ingresa un nombre para la sala:")
-    
     if st.button("Crear Sala"):
         if sala_id:
-            codigo_sala = random.randint(1000, 9999)  # Genera un código de 4 dígitos
-            codigo_sala = str(codigo_sala)
+            codigo_sala = str(hash(sala_id))
             salas[codigo_sala] = {"nombre_sala": sala_id, "mensajes": []}
-            
-            # Ventana emergente de bienvenida
             st.balloons()
-            st.write(f"Bienvenido a la Sala '{sala_id}'")
-            st.write(f"Tu código de sala es: {codigo_sala}")
-
+            st.success(f"Sala '{sala_id}' creada con éxito. Código de sala: {codigo_sala}")
+            
+            # Agrega un retraso de 5 segundos
+            with st.empty():
+                for i in range(5):
+                    st.progress(i / 5)
+                    time.sleep(1)
+                st.empty()  # Borra la barra de progreso
+                
+                # Redirecciona a una nueva página
+                st.experimental_rerun()
+            
 elif accion == "Unirse a una sala":
     codigo_ingresado = st.text_input("Ingresa el código de la sala:")
     if st.button("Unirse a Sala"):
         if codigo_ingresado in salas:
             st.balloons()
             st.success(f"Te has unido a la sala '{salas[codigo_ingresado]['nombre_sala']}'")
+            # Agrega aquí la redirección a una nueva página para solicitar el nombre y avatar.
 
 # Chat en la sala (puede ser mejorado para incluir más características)
 if accion == "Unirse a una sala" and codigo_ingresado in salas:
@@ -38,5 +51,6 @@ if accion == "Unirse a una sala" and codigo_ingresado in salas:
     if st.button("Enviar"):
         if mensaje:
             sala_actual["mensajes"].append(f"Usuario: {mensaje}")
+
 
 
